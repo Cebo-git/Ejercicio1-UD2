750679a739be65bdbfc1bad3efb4766c3dfdd994
Victor Cebollada <vic.cebollada@gmail.com>
2024-11-28 16:26:02 +0100

    Añado proyecto2

---
+++ Proyecto2/src/sumando/Sumando.java
@@ -0,0 +1,14 @@
+package sumando;
+
+import java.util.Scanner;
+
+public class Sumando {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce el primer número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Introduce el segundo número: ");
+        int num2 = scanner.nextInt();
+        System.out.println("La suma es: " + (num1 + num2));
+    }
+}
